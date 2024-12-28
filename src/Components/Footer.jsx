0d6944ed4cb3d8298e1e75e0d2f87bc8e064c14b
Aitezaz Sikandar <aitezazsikandar@gmail.com>
2024-12-28 12:09:56 +0500

    update footer

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const Footer = () => {
   return (
-    <footer className="bg-zinc-900 text-white py-6">
+    <footer className="py-10" style={{ boxShadow: "0px -1.2px 2px rgba(0, 0, 0, 0.1)" }}>
       <div className="container mx-auto text-center">
         <h2 className="text-lg font-semibold mb-2">
           Made with ❤️ by <a href="https://github.com/aitezazdev" target="_blank" rel="noopener noreferrer" className="text-indigo-500 hover:underline">aitezaz</a>

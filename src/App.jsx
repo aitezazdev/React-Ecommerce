c3029e2653637f225becdff88ee1f27a20fa820f
Aitezaz Sikandar <aitezazsikandar@gmail.com>
2025-01-03 19:46:07 +0500

    routes fix

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import Footer from "./Components/Footer";
 import Navbar from "./Components/Navbar";
-import { Route, Routes } from "react-router-dom";
+import { Route, Router, Routes } from "react-router-dom";
 import HomePage from "./Pages/HomePage";
 import ProductsPage from "./Pages/ProductsPage";
 import ContactPage from "./Pages/ContactPage";
@@ -12,7 +12,6 @@ import ProductDetails from "./Components/ProductDetails";
 import Checkout from "./Pages/CheckoutPage";
 
 function App() {
-
   return (
     <div className="flex flex-col min-h-screen">
       <ScrollToTop />
@@ -22,14 +21,16 @@ function App() {
       </header>
 
       <main className="flex-grow p-2">
-        <Routes>
-          <Route path="/" index element={<HomePage />} />
-          <Route path="/products" element={<ProductsPage />} />
-          <Route path="/contact" element={<ContactPage />} />
-          <Route path="/cart" element={<CartPage />} />
-          <Route path="/product-details/:id" element={<ProductDetails />} />
-          <Route path="/checkout" element={<Checkout />} />
-        </Routes>
+        <Router basename="/React-Ecommerce">
+          <Routes>
+            <Route path="/" index element={<HomePage />} />
+            <Route path="/products" element={<ProductsPage />} />
+            <Route path="/contact" element={<ContactPage />} />
+            <Route path="/cart" element={<CartPage />} />
+            <Route path="/product-details/:id" element={<ProductDetails />} />
+            <Route path="/checkout" element={<Checkout />} />
+          </Routes>
+        </Router>
       </main>
 
       <footer>
